d9b79b4f6505b3969ce1b3c45812de727db17a91
driesvd22 <driesvandievoort8@gmail.com>
2018-05-08 10:57:39 +0200

    First Commit

---
+++ Dries_Van_Dievoort_S2IT_werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dries_Van_Dioevoort_S2IT_werkstuk1
+//
+//  Created by student on 08/05/18.
+//  Copyright © 2018 EHB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
